6e78a71dd640500ff760fbbe68710b12345264cd
Anastasia Yarosh <fysyad@gmail.com>
2025-02-25 15:47:39 +0300

    Удалила лишнее из ридми, написала тип карточки, прокинула пропсы в карточки

--- src/components/TheCard.vue
+++ src/components/TheCard.vue
@@ -1,8 +1,18 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { ICard } from '@/types/common';
+
+const props = defineProps<{
+  card: ICard
+}>();
+const card = ref(props.card);
+</script>
 
 <template>
+  {{ card }}
   <div>
-    <div>number</div>
+    {{ card.number  }}
+    {{ card.text }}
   </div>
 </template>
 
